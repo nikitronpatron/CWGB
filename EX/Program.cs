1cf7f4e2cc0c3e572b090554bbecb51f3e9f6031
nikitronpatron <nikita.borsh9876@gmail.com>
2022-12-07 21:57:38 +0300

    Wrote program

--- EX/Program.cs
+++ EX/Program.cs
@@ -2,3 +2,28 @@
 // массива строк формирует массив из строк
 // длина которых меньше либо равна 3.
 
+string[] myArray = {"Hello", "234", "21", "win", "eh", "326", "-2"};
+
+void ShowArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void ProcessedArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        string temp = array[i];
+        if(temp.Length < 4)
+        {
+            Console.Write($"{temp} ");
+        }
+    }
+}
+
+ShowArray(myArray);
+ProcessedArray(myArray);
